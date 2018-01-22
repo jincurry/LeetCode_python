41a8ad03bdc905df83f7658f08061a93cb01762c
jincurry <jintao422516@gmail.com>
2018-01-22 16:57:18 +0800

    添加对168_Excel_Sheet_Column_Title的解答

---
+++ 168_Excel Sheet Column Title.py
@@ -0,0 +1,32 @@
+# Given a positive integer, return its corresponding
+#  column title as appear in an Excel sheet.
+#
+# For example:
+#
+#     1 -> A
+#     2 -> B
+#     3 -> C
+#     ...
+#     26 -> Z
+#     27 -> AA
+#     28 -> AB
+
+
+class Solution:
+    def convertToTitle(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        result, dvd = "", n
+
+        while dvd:
+            result += chr((dvd - 1) % 26 + ord('A'))
+            dvd = (dvd - 1) // 26
+
+        return result[::-1]
+
+if __name__ == '__main__':
+    solution = Solution()
+    for i in range(1,234):
+        print(solution.convertToTitle(i))
