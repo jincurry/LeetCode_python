6e592de3502ddae6c905ee7fb50b2f5da82877be
jincurry <jintao422516@gmail.com>
2018-01-25 10:14:59 +0800

    添加对204_Count_Primes的解答

---
+++ 204_Count Primes.py
@@ -0,0 +1,24 @@
+# Description:
+#
+# Count the number of prime numbers less than a non-negative number, n.
+
+
+class Solution:
+    def countPrimes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n < 3:
+            return 0
+        primes = [True] * n
+        primes[0] = primes[1] = False
+        for i in range(2, int(n ** 0.5) + 1):
+            if primes[i]:
+                primes[i * i: n: i] = [False] * len(primes[i * i: n: i])
+        return sum(primes)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.countPrimes(10))
