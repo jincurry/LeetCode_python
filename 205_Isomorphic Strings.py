115498cfa5ae9f0b7f14ad5c373605f20c67fb0b
jincurry <jintao422516@gmail.com>
2018-01-25 10:33:42 +0800

    添加对205_Isomorphic_Strings的解答

---
+++ 205_Isomorphic Strings.py
@@ -0,0 +1,39 @@
+# Given two strings s and t, determine if they are isomorphic.
+#
+# Two strings are isomorphic if the characters in s can be replaced to get t.
+#
+# All occurrences of a character must be replaced with another character while preserving the order of characters.
+#  No two characters may map to the same character but a character may map to itself.
+#
+# For example,
+# Given "egg", "add", return true.
+#
+# Given "foo", "bar", return false.
+#
+# Given "paper", "title", return true.
+
+
+class Solution:
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+
+        s2t, t2s = {}, {}
+        for p, w in zip(s, t):
+            if w not in s2t and p not in t2s:
+                s2t[w] = p
+                t2s[p] = w
+            elif w not in s2t or s2t[w] != p:
+                # Contradict mapping.
+                return False
+        return True
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.isIsomorphic('egg', 'add'))
