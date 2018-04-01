e371859d69c15a848adf003595bba68d8dd01e0f
jincurry <jintao422516@gmail.com>
2018-04-01 18:32:52 +0800

    添加对371_sum_of_two_integers的解答

---
+++ 371_sum_of_two_integers.py
@@ -0,0 +1,42 @@
+# Calculate the sum of two integers a and b, but you are not allowed
+# to use the operator + and -.
+#
+# Example:
+# Given a = 1 and b = 2, return 3.
+
+
+class Solution:
+    def getSum(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+
+
+        # sum = a ^ b
+        # carry = (a & b) << 1
+        # while carry != 0:
+        #     k = sum
+        #     v = carry
+        #     sum = k ^ v
+        #     carry = (k & v) << 1
+        # return sum
+
+        # 32 bits integer max
+        MAX = 0x7FFFFFFF
+        # 32 bits interger min
+        MIN = 0x80000000
+        # mask to get last 32 bits
+        mask = 0xFFFFFFFF
+        while b != 0:
+            # ^ get different bits and & gets double 1s, << moves carry
+            a, b = (a ^ b) & mask, ((a & b) << 1) & mask
+        # if a is negative, get a's 32 bits complement positive first
+        # then get 32-bit positive's Python complement negative
+        return a if a <= MAX else ~(a ^ mask)
+
+if __name__ == '__main__':
+    print(Solution().getSum(1, 2))
+    print(Solution().getSum(23, 5))
+    print(Solution().getSum(25, 67))
