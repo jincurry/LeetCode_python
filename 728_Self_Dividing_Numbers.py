163394f278146aacac9821260f8171a2af83da50
jincurry <jintao422516@gmail.com>
2018-06-06 17:25:46 +0800

    添加对728_Self_Dividing_Numbers的解答

---
+++ 728_Self_Dividing_Numbers.py
@@ -0,0 +1,43 @@
+# A self-dividing number is a number that is divisible by every digit it contains.
+#
+# For example, 128 is a self-dividing number because 128 % 1 == 0, 128 % 2 == 0, and 128 % 8 == 0.
+#
+# Also, a self-dividing number is not allowed to contain the digit zero.
+#
+# Given a lower and upper number bound, output a list of every possible self dividing number,
+# including the bounds if possible.
+#
+# Example 1:
+# Input:
+# left = 1, right = 22
+# Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22]
+
+
+class Solution:
+    def selfDividingNumbers(self, left, right):
+        """
+        :type left: int
+        :type right: int
+        :rtype: List[int]
+        """
+        result = []
+
+        def selfDividing(nums):
+            c = nums
+            while c != 0:
+                d = c % 10
+                if d == 0:
+                    return False
+                if nums % d != 0:
+                    return False
+                c = c // 10
+            return True
+        for num in range(left, right+1):
+            if selfDividing(num):
+                result.append(num)
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.selfDividingNumbers(1, 22))
